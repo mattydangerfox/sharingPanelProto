fec738d5c006a1d8da8f359f9041fccdcac16f97
Sanghee Kim <sangheestyle@gmail.com>
2017-07-12 15:00:45 -0400

    removePanel mutation on UI

--- client/src/components/Panel.js
+++ client/src/components/Panel.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import { gql, graphql } from 'react-apollo';
+import { panelsQuery } from './DashBoard';
 
 class Panel extends Component {
+  _handleOnClick = (e) => {
+    this.props.mutate({
+      variables: {
+        id: this.props.panel.id
+      },
+      update: (store, { data: { removePanel } }) => {
+        const data = store.readQuery({
+          query: panelsQuery,
+        });
+        const index = data.panels.findIndex(p => p.id === this.props.panel.id);
+        if (index !== -1) {
+          data.panels.splice(index, 1);
+        }
+        store.writeQuery({
+          query: panelsQuery,
+          data
+        });
+      }
+    })
+  };
+
   render() {
     const {id, title} = this.props.panel;
     return (
-      <li>id: {id}, title: {title}</li>
+      <li>id: {id}, title: {title} <button type="button" onClick={this._handleOnClick}>remove</button></li>
     );
   }
 }
 
-export default Panel;
+const removePanelMutation = gql`
+  mutation removePanel($id: ID!) {
+      removePanel(id: $id) {
+          id
+      }
+  }
+`;
+
+const PanelWithMutation = graphql(removePanelMutation)(Panel);
+export default PanelWithMutation;
