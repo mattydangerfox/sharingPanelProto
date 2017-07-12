17f7241f3e8bb588b203f39c78eeb160d73a895a
Sanghee Kim <sangheestyle@gmail.com>
2017-07-12 16:10:04 -0400

    edit title on UI
    Reference:
    http://dev.apollodata.com/react/mutations.html#multiple-mutations

--- client/src/components/Panel.js
+++ client/src/components/Panel.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import { gql, graphql } from 'react-apollo';
+import { gql, graphql, compose } from 'react-apollo';
 import { panelsQuery } from './DashBoard';
 
+const ENTER_KEY_CODE = 13;
+const ESC_KEY_CODE = 27;
+
 class Panel extends Component {
-  _handleOnClick = (e) => {
-    this.props.mutate({
+  state = {
+    editing: false
+  };
+
+  _handleOnClickRemove = (e) => {
+    this.props.removePanelMutation({
       variables: {
         id: this.props.panel.id
       },
@@ -30,10 +37,47 @@ class Panel extends Component {
     })
   };
 
+  _handleOnClickEdit = (e) => {
+    this.setState({editing: true});
+  };
+
+  _handleKeyDownOnInput = (e) => {
+    if(e.keyCode === ESC_KEY_CODE) {
+      this.setState({editing: false});
+    } else if (e.keyCode === ENTER_KEY_CODE) {
+      this.props.updatePanelMutation({
+        variables: {
+          input: {id: this.props.panel.id, title: e.target.value}
+        },
+        update: (store, { data: { updatePanel } }) => {
+          const data = store.readQuery({
+            query: panelsQuery,
+          });
+          const index = data.panels.findIndex(p => p.id === this.props.panel.id);
+          data.panels[index] = updatePanel;
+          store.writeQuery({
+            query: panelsQuery,
+            data
+          });
+        }
+      });
+      this.setState({editing: false});
+    }
+  };
+
   render() {
     const {id, title} = this.props.panel;
+    if (this.state.editing) {
+      return (
+        <li><input placeholder={title} onKeyDown={this._handleKeyDownOnInput}/></li>
+      );
+    }
+
     return (
-      <li>id: {id}, title: {title} <button type="button" onClick={this._handleOnClick}>remove</button></li>
+      <li>id: {id}, title: {title}
+        <button type="button" onClick={this._handleOnClickEdit}>edit</button>
+        <button type="button" onClick={this._handleOnClickRemove}>remove</button>
+      </li>
     );
   }
 }
@@ -46,5 +90,17 @@ const removePanelMutation = gql`
   }
 `;
 
-const PanelWithMutation = graphql(removePanelMutation)(Panel);
+const updatePanelMutation = gql`
+    mutation updatePanel($input: UpdatePanelInput!) {
+        updatePanel(input: $input) {
+            id
+            title
+        }
+    }
+`;
+
+const PanelWithMutation = compose(
+  graphql(removePanelMutation,{ name: 'removePanelMutation' }),
+  graphql(updatePanelMutation, { name: 'updatePanelMutation' }),
+)(Panel);
 export default PanelWithMutation;
