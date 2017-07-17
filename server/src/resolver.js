f2591ff338e097a6d91debf1446ac13eda5149f5
Sanghee Kim <sangheestyle@gmail.com>
2017-07-17 17:35:59 -0400

    mutation sharePanel
    TODO:
    
    a. DB part should be changed later.
    b. ownerId should be retrieved from JWT.

--- server/src/resolver.js
+++ server/src/resolver.js
@@ -60,6 +60,12 @@ panels.push(panelB);
 export const resolvers = {
   Query: {
     panels: ( object, { input: { userId }} ) => {
+      if (userId === 'admin') {
+        return {
+          panels: panels
+        };
+      }
+
       return {
         panels: panels.filter(panel => panel.owner.id === userId)
       };
@@ -68,6 +74,32 @@ export const resolvers = {
       return {
         panelData: getPanelData(query)
       }
+    }
+  },
+  Mutation: {
+    sharePanel: ( object, {input: { panelQueryId, ownerId, sharedWith }} ) => {
+      const panelIndex = panels.findIndex(panel => panel.panelQuery.id.toString() === panelQueryId);
+      const alreadyShared = panels[panelIndex].panelQuery.sharedWith.findIndex(sw => sw.id === sharedWith);
+
+      if (panels[panelIndex].panelQuery.owner.id !== ownerId) {
+        throw new Error('You are not the owner of the panelQuery.');
+      }
+      if (alreadyShared !== -1) {
+        throw new Error(`PanelQuery ${panelQueryId} is already shared with user ${sharedWith}.`);
+      }
+
+      panels[panelIndex].panelQuery.sharedWith.push({id: sharedWith});
+      const newPanel = {
+        id: ++panelCounter,
+        owner: {id: sharedWith},
+        panelShape: defaultPanelShape,
+        panelQuery: panels[panelIndex].panelQuery,
+      };
+      panels.push(newPanel);
+
+      return {
+        panelQuery: panels[panelIndex].panelQuery
+      };
     }
   }
 };
