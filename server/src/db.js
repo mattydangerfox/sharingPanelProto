8a3c30856ac20abe2b63bc85f26240fd62b0ab83
Sanghee Kim <sangheestyle@gmail.com>
2017-07-18 19:37:27 -0400

    create panel and panelQuery

--- server/src/db.js
+++ server/src/db.js
@@ -1,7 +1,41 @@
 const db = new Map();
 const tabelNames = ['panel', 'panelShape', 'panelQuery'];
+const counter = {
+  panelCounter: 0,
+  panelShapeCounter: 0,
+  panelQueryCounter: 0,
+};
 
 tabelNames.map(table => db.set(table, new Map()));
 
+const createPanelQuery = ({ owner, query }) => {
+  counter.panelQueryCounter =+ 1;
+  const newPanelQuery = {
+    owner,
+    id: counter.panelQueryCounter,
+    sharedWith: [],
+    esQuery: {
+      query
+    }
+  };
+  db.get('panelQuery').set(counter.panelQueryCounter, newPanelQuery);
+  return newPanelQuery;
+};
+
+const createPanel = ({ owner, query }) => {
+  counter.panelCounter += 1;
+  const newPanel = {
+    owner,
+    id: counter.panelCounter,
+    panelQuery: createPanelQuery({owner, query}),
+    panelShape: {
+      height: 500,
+      width: 600,
+    }
+  };
+  db.get('panel').set(counter.panelCounter, newPanel);
+  return newPanel;
+};
+
 export default db;
-export {tabelNames};
+export { tabelNames, createPanelQuery, createPanel };
--- server/src/db.test.js
+++ server/src/db.test.js
@@ -1,5 +1,49 @@
-import db, { tabelNames } from './db'
+import db, {
+  tabelNames,
+  createPanelQuery,
+  createPanel,
+} from './db'
 
 test('db has three tables.', () => {
   expect(Array.from(db.keys())).toEqual(tabelNames);
+});
+
+test('creating panelQuery returns panelQuery object.', () => {
+  const owner = {
+    id: 1
+  };
+  const query = 'Summer Trend 2016';
+  const panelQuery = {
+    owner,
+    id: 1,
+    sharedWith: [],
+    esQuery: {
+      query
+    }
+  };
+  expect(createPanelQuery({ owner, query })).toEqual(panelQuery);
+});
+
+test('creating panel returns panel object.', () => {
+  const owner = {
+    id: 1
+  };
+  const query = 'Summer Trend 2016';
+  const panel = {
+    owner,
+    id: 1,
+    panelShape: {
+      height: 500,
+      width: 600,
+    },
+    panelQuery: {
+      owner,
+      id: 1,
+      sharedWith: [],
+      esQuery: {
+        query
+      }
+    }
+  };
+  expect(createPanel({owner, query})).toEqual(panel);
 });
