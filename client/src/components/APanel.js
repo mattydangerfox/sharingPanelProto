50da97fa11155d2459c1c44ef741cb7f765b1677
Sanghee Kim <sangheestyle@gmail.com>
2017-07-21 22:55:11 -0400

    DRAFT: middle of implementing edit query

--- client/src/components/APanel.js
+++ client/src/components/APanel.js
@@ -2,12 +2,61 @@ import React, { Component } from 'react';
 import {
   gql,
   graphql,
+  compose,
 } from 'react-apollo';
 
+const ENTER_KEY_CODE = 13;
+const ESC_KEY_CODE = 27;
+
 class APanel extends Component {
+  state = {
+    editing: false
+  };
+
+  _handleOnClickEdit = (e) => {
+    this.setState({editing: true});
+  };
+
+  // TODO
+  _handleKeyDownOnInput = (e) => {
+    if(e.keyCode === ESC_KEY_CODE) {
+      this.setState({editing: false});
+    } else if (e.keyCode === ENTER_KEY_CODE) {
+      /*
+      this.props.updatePanelMutation({
+        variables: {
+          input: {
+            id: this.props.panel.id, title: e.target.value}
+        },
+        update: (store, { data: { updatePanel } }) => {
+          const data = store.readQuery({
+            query: panelsQuery,
+            variables: { userId: this.props.userId }
+          });
+          const index = data.panels.findIndex(p => p.id === this.props.panel.id);
+          data.panels[index] = updatePanel;
+          store.writeQuery({
+            query: panelsQuery,
+            data
+          });
+        }
+      });
+      */
+      this.setState({editing: false});
+    }
+  };
+
   render() {
     const { data: { loading, error, search }, panel } = this.props;
-    let text = `panel.id: ${panel.id}, query: ${panel.panelQuery.esQuery.query} - `;
+    const query = panel.panelQuery.esQuery.query;
+    if (this.state.editing) {
+      const input = <input
+        placeholder={query}
+        onKeyDown={this._handleKeyDownOnInput} />;
+      return <li>{input}</li>;
+    }
+
+    let text = `panel.id: ${panel.id}, query: ${query} - `;
     if (loading) {
       text += 'loading...';
       return <li>{text}</li>;
@@ -19,7 +68,7 @@ class APanel extends Component {
     }
 
     text += `Result: ${search.panelData.series[0].data.toString()}`;
-    return <li>{text}</li>;
+    return <li>{text}<button type="button" onClick={this._handleOnClickEdit}>edit query</button></li>;
   }
 }
 
@@ -38,8 +87,33 @@ export const searchQuery = gql`
   }
 `;
 
-export default(graphql(searchQuery, {
-  options: ({ panel }) => ({
-    variables: { input: { query: panel.panelQuery.esQuery.query }}
-  })
-}))(APanel);
+// FIXME
+const updatePanelMutation = gql`
+    mutation updatePanel($input: UpdatePanelInput!) {
+        updatePanel(input: $input) {
+            id
+            owner
+            title
+            panelData {
+                title {
+                    text
+                }
+                xAxis {
+                    categories
+                }
+                series {
+                    data
+                }
+            }
+        }
+    }
+`;
+
+export default(compose(
+  graphql(searchQuery, {
+    options: ({panel}) => ({
+      variables: {input: {query: panel.panelQuery.esQuery.query}}
+    }),
+  }),
+  graphql(updatePanelMutation, { name: 'updatePanelMutation' }),
+))(APanel);
